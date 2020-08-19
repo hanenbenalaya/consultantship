e0199c3a5c9c513fd5ad0ae4ae9ed504a2204586
benalaya hanen <hanen.benalaya37@gmail.com>
2020-08-19 21:26:21 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LandingpageComponent
+
+  },
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { PartenairesComponent } from './components/partenaires/partenaires.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LandingpageComponent,
+    PartenairesComponent
   ],
   imports: [
     BrowserModule,
